41fb13d05a91871e9fbc503b5af5a9c88d569b8f
kmaune0 <kmaune71@gmail.com>
2014-06-07 13:54:57 -0700

    simple payroll program

---
+++ PayrollProgramPart2.java
@@ -0,0 +1,54 @@
+// PayrollProgramPart2.java
+// IT 215 Week 3 Assignment
+// Copyright (C) 2014 Kyle Maune
+
+
+import java.util.Scanner;
+
+public class PayrollProgramPart2
+{
+
+	public static void main(String args[])
+	{
+
+		Scanner input = new Scanner(System.in);
+
+			String cleanInputBuffer;
+			String employeeName;
+			double hourlyRate;
+			double hoursWorked;
+			double sum;
+			boolean end = false;
+
+		while (end == false)
+		{
+			hourlyRate = -1;
+			hoursWorked = -1;
+
+			System.out.println("Welcome to the Payroll Program!\n");
+
+			System.out.println("Enter Name of Employee(enter 'stop' to exit program): ");
+				employeeName = input.nextLine();
+
+			if ("stop".equals(employeeName))
+				end = true;
+			else
+				System.out.println("Enter a positive hourly rate:");
+					hourlyRate = input.nextDouble();
+
+			while (hoursWorked < 0)
+			{
+				System.out.println("Enter a positive number of hours worked:");
+					hoursWorked = input.nextDouble();
+			}
+			sum = hourlyRate * hoursWorked;
+
+			System.out.printf("The employee %s was paid $ %.2f this week\n\n", employeeName, sum);
+
+		cleanInputBuffer = input.nextLine();
+
+		}
+
+	}
+
+}
